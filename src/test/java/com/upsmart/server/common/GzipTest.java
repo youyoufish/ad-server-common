8a4a0c4aa610ee78c30bbe359de62cabe4e9ac63
yuhang <yuhang@unionpaysmart.com>
2017-05-02 10:31:47 +0800

    ut

--- src/test/java/com/upsmart/server/common/GzipTest.java
+++ src/test/java/com/upsmart/server/common/GzipTest.java
@@ -4,18 +4,32 @@ import com.upsmart.server.common.codec.Gzip;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.*;
+
 /**
  * Created by yuhang on 17-5-2.
  */
 public class GzipTest {
 
     @Test
-    public void testGzip(){
+    public void testStringGzip(){
 
         String str = "{[ABC:456]}为统一台湾而奋斗!";
         String encoding = "utf-8";
         byte[] text = Gzip.compressToByte(str, encoding);
         String result = Gzip.uncompressToString(text, encoding);
+        Assert.assertEquals(str, result);
+    }
+
+    @Test
+    public void testStreamGzip() throws IOException {
+
+        String str = "{[ABC:456]}为统一台湾而奋斗!";
+        String encoding = "utf-8";
+        byte[] text = Gzip.compressToByte(str, encoding);
+
+        InputStream is = new ByteArrayInputStream(text);
+        String result = Gzip.uncompressToString(is, encoding);
         Assert.assertEquals(str, result);
     }
 }
